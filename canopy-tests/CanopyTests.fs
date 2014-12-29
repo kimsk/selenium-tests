3d0f1164f4b8c731e44e9c0dbacffc5ac7b62149
Karlkim Suwamongkol <contact@karlk.im>
2014-12-29 10:20:11 -0800

    Try Canopy

---
+++ canopy-tests/CanopyTests.fs
@@ -0,0 +1,14 @@
+﻿module CanopyTests
+
+open Xunit
+open FsUnit.Xunit
+open canopy
+open System
+
+[<Fact>]
+let ``open google`` () =
+    start chrome
+
+    url "http://www.google.com"
+
+    quit()
